76be08b4dad06cf58fcc4386e47398bdf037a767
gabrielgrs <grxgabriel@gmail.com>
2018-08-13 16:24:33 -0300

    ajustes de vulnerabilidade

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
-    connectionString: 'mongodb://admin:admin@ds113505.mlab.com:13505/gabrielgrsdb',
+    connectionString: 'mongodb://admin:admin@ds123956.mlab.com:23956/generics',
     sendgridKey: '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',
     containerConnectionString: 'TDB'
 }
